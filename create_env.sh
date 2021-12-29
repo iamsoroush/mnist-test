6219229c3913449b7151821f400c131b3637d69d
soroush <soroush.moazed@gmail.com>
2021-12-29 11:09:51 +0330

    Add mnist data

---
+++ create_env.sh
@@ -0,0 +1,5 @@
+conda activate aimed
+conda update pip
+pip install -r requirements.txt
+#conda install pandas scipy scikit-learn scikit-image matplotlib pyyaml tqdm
+#pip install tensorflow mlflow simpleitk abstractions-aimedic albumentations pytest pytest-dependency
